47e98d61fb5b0d1652459bbc1395fc33e898e58e
Roberta D'Agostino <robertadag02@gmail.com>
2022-09-11 17:23:15 +0200

    fix: responsiveness improved

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MovieEntity from './components/MovieEntity'
 import MainSection from './components/MainSection'
 import MainInput from './components/MainInput'
 import Navbar from './components/Navbar'
+
 import './App.scss';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <MovieEntity movieID={inputValue}/>
       <MainInput setInputValue={setInputValue} />
       <MainSection />
+     
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,22 @@
-import styles from './index.module.scss';
+
+import styles from "./index.module.scss";
 
 const Navbar = () => {
+ 
+
+
   return (
     <div className={styles.Navbar}>
       <ul className={styles.list}>
         <li>
-        <a href="#main-section">Popular Films</a>
+          <a href="#main-section">Popular Films</a>
         </li>
         <li>
-        <a href="#top-rated">Top Rated</a>
+          <a href="#top-rated">Top Rated</a>
         </li>
       </ul>
+     
+   
     </div>
   );
 };
